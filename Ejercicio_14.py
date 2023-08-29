586da250acd9cb9ff5262db23602d15c174fab14
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-08-29 15:19:59 -0500

    Create Ejercicio_14.py

---
+++ Ejercicio_14.py
@@ -0,0 +1,10 @@
+# Este programa solicita al usuario que ingrese un número y luego calcula su cuadrado y su cubo.
+# Solicitamos al usuario ingresar un número.
+nu = float(input("Ingrese un número: "))
+# Calculamos el cuadrado del número ingresado elevándolo al cuadrado.
+r_cuadrado = nu ** 2
+# Calculamos el cubo del número ingresado elevándolo al cubo.
+r_cubo = nu ** 3
+# Mostramos los resultados.
+print("El cuadrado de", nu, "es", r_cuadrado)  # Mostramos el resultado del cuadrado del número.
+print("El cubo de", nu, "es", r_cubo)  # Mostramos el resultado del cubo del número.
